ad186a9f9ea4a382d5261f20092f2de8d71e92d5
Hugo <hugoram78@gmail.com>
2017-08-11 12:41:24 -0500

    Set launch screen and initial view.

--- Proximity/AppDelegate.swift
+++ Proximity/AppDelegate.swift
@@ -15,8 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        print("launched")
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = ColorViewController(nibName: "ColorViewController", bundle: nil)
+        self.window?.makeKeyAndVisible()
         return true
     }
 
---
+++ Proximity/ColorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ColorViewController.swift
+//  Proximity
+//
+//  Created by hugo on 8/11/17.
+//  Copyright © 2017 hugo. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
