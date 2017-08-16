9a5e7232274ff672e80905083b8154214b9688a8
Hugo <hugoram78@gmail.com>
2017-08-16 14:24:02 -0500

    Add particle system with custom style.

--- Proximity/ParticlesViewController.swift
+++ Proximity/ParticlesViewController.swift
@@ -12,9 +12,48 @@ import SpriteKit
 class ParticlesViewController: UIViewController {
 
     @IBOutlet weak var skview: SKView!
+    var scene: SKScene?
+    var emitter: SKEmitterNode?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupScene()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.scene?.size = self.skview.bounds.size
+        self.emitter?.position = self.skview.center
+    }
+
+    func setupScene() {
+        self.scene = SKScene(size: self.skview.bounds.size)
+        self.scene?.scaleMode = SKSceneScaleMode.aspectFill
+        self.scene?.backgroundColor = UIColor.black
+        self.skview.presentScene(self.scene)
+
+        self.emitter = SKEmitterNode()
+        self.emitter?.particleTexture = SKTexture(imageNamed: "particle1.png")
+        self.emitter?.particleBlendMode = SKBlendMode.add
+        self.emitter?.particleColorSequence = SKKeyframeSequence(keyframeValues: [
+            UIColor.orange,
+            UIColor.white,
+            UIColor.orange
+        ], times: [0, 0.6, 1])
+        self.emitter?.particleColorBlendFactor = 1
+        self.emitter?.particleColorRedRange = 100
+        self.emitter?.particleColorRedRange = 1
+        self.emitter?.particlePositionRange = CGVector(dx: 0, dy: 240)
+        self.emitter?.particleBirthRate = 50
+        self.emitter?.particleLifetime = 2
+        self.emitter?.particleLifetimeRange = 1
+        self.emitter?.particleSpeed = 60
+        self.emitter?.particleSpeedRange = 50
+        self.emitter?.particleAlphaSequence = SKKeyframeSequence(keyframeValues: [1, 0.9, 0], times: [0, 0.8, 1])
+        self.emitter?.emissionAngle = 0
+        self.emitter?.emissionAngleRange = CGFloat.pi * 2
+        self.emitter?.particleScaleSequence = SKKeyframeSequence(keyframeValues: [0, 3.6, 0], times: [0, 0.08, 1])
+        self.scene?.addChild(self.emitter!)
     }
 
     func updateSignal(strength:Int) {
