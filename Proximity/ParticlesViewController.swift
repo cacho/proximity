0b0a0bd417736e3570305def7c1ea0a78c0052d8
Hugo <hugoram78@gmail.com>
2017-08-16 14:34:11 -0500

    Link particle birth rate to device signal strength.

--- Proximity/ParticlesViewController.swift
+++ Proximity/ParticlesViewController.swift
@@ -14,6 +14,7 @@ class ParticlesViewController: UIViewController {
     @IBOutlet weak var skview: SKView!
     var scene: SKScene?
     var emitter: SKEmitterNode?
+    var offTimer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,7 +45,7 @@ class ParticlesViewController: UIViewController {
         self.emitter?.particleColorRedRange = 100
         self.emitter?.particleColorRedRange = 1
         self.emitter?.particlePositionRange = CGVector(dx: 0, dy: 240)
-        self.emitter?.particleBirthRate = 50
+        self.emitter?.particleBirthRate = 0
         self.emitter?.particleLifetime = 2
         self.emitter?.particleLifetimeRange = 1
         self.emitter?.particleSpeed = 60
@@ -58,5 +59,21 @@ class ParticlesViewController: UIViewController {
 
     func updateSignal(strength:Int) {
         print(strength)
+        let max = -55.0
+        let min = -80.0
+        let maxP = max + min * -1
+        let normalized = Double(strength) + min * -1
+        var scaled = normalized / maxP
+        if scaled > 1.0 {
+            scaled = 1.0
+        } else if scaled < 0.0 {
+            scaled = 0.0
+        }
+        self.emitter?.particleBirthRate = CGFloat(60 * scaled)
+
+        self.offTimer?.invalidate()
+        self.offTimer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { (timer) in
+            self.emitter?.particleBirthRate = 0
+        }
     }
 }
